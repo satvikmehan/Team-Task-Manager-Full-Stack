f1a6947946c5e01d851de14e9e22d57731936c74
satvikmehan <satvikmehan@gmail.com>
2026-05-03 02:24:39 +0530

    add home route

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from .views import home
 from django.urls import path, include
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('projects/', include('projects.urls')), 
---
+++ core/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Task Manager API is running 🚀"
+    })
